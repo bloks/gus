0524acc895a427b1a78d181c97bbcf8ff4c11c7b
bloks <blok_sergey@bah.com>
2024-12-22 17:52:49 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import webbrowser
+import streamlit.components.v1 as components
+
+st.title('Endless Gus')
+
+# target_url = 'player.html'
+# # embed streamlit docs in a streamlit app
+# components.iframe(target_url)
+
+target_url = 'https://wfmu.org/archiveplayer/?show=95483&archive=189777&starttime=0:44:0'
+webbrowser.open_new_tab(target_url)
+
+
+
