2c6a34ef07a1a0db0b8e3c5cf8441ada85b8edc5
Sergey Blok <blok_sergey@bah.com>
2024-12-22 18:02:08 -0500

    updated app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import streamlit.components.v1 as components
 
 st.title('Endless Gus')
 
-# target_url = 'player.html'
-# # embed streamlit docs in a streamlit app
-# components.iframe(target_url)
+target_url = 'player.html'
+# embed streamlit docs in a streamlit app
+components.iframe(target_url)
 
-target_url = 'https://wfmu.org/archiveplayer/?show=95483&archive=189777&starttime=0:44:0'
-webbrowser.open_new_tab(target_url)
+# target_url = 'https://wfmu.org/archiveplayer/?show=95483&archive=189777&starttime=0:44:0'
+# webbrowser.open_new_tab(target_url)
 
 
 
