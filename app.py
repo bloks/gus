48d797c70b2c3a95875fbbd6c3a940a4295f79ed
Sergey Blok <blok_sergey@bah.com>
2024-12-22 19:52:06 -0500

    safari

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
+driver = webdriver.Safari()
 
 st.title('Endless Gus')
 
