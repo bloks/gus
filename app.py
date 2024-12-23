8a7e2923ce451323494e0c77ac9259698806a92e
Sergey Blok <blok_sergey@bah.com>
2024-12-22 19:50:51 -0500

    selenium approach

--- app.py
+++ app.py
@@ -3,20 +3,46 @@ import pandas as pd
 import numpy as np
 import webbrowser
 import streamlit.components.v1 as components
+from selenium.webdriver.common.by import By
+import time
+
+
+#driver.find_elements(By.XPATH, '//button')
+
+
+#%pip install selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
 
 st.title('Endless Gus')
 
+player_url = "https://wfmu.org/archiveplayer/?show=95483&archive=189777&starttime=0:44:0"
+driver.get(player_url)
 
-html_string = "<span style=""white-space:nowrap""><a href=""https://wfmu.org/flashplayer.php?version=3&amp;show=140083&amp;archive=253112&amp;starttime=0:30:21"" onclick=""return kdbarchivelink(this.href, 3)"">Pop-up</a></span>"
+play_button = driver.find_element(By.XPATH, '//button')
 
-st.html(html_string)
 
+#play_button.click() #start
+
+
+
+time.sleep(10)
+
+
+play_button.click() #stop
 
 
 
 
 # target_url = 'https://wfmu.org/archiveplayer/?show=95483&archive=189777&starttime=0:44:0'
 # webbrowser.open_new_tab(target_url)
+
+
+
+
 
 
 
